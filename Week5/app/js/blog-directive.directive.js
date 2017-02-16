2414b119dfd2fe8cec62283a58bdfad73e8f252f
webdesign account <webdesign@dublindesign.ie>
2017-02-16 20:50:57 +0000

    things

--- Week5/app/js/blog-directive.directive.js
+++ Week5/app/js/blog-directive.directive.js
@@ -5,9 +5,7 @@ angular.module('app').directive('blogDirective', function () {
 		scope: {
 			article: "="
 		},
-        template: `<h3>{{article.title}}</h3> 
-                        <h4>by {{article.author}}</h4> 
-                            <p class="text-justify">{{article.articleBlog}}</p>`,
+        templateUrl: '../js/blog_template.html',
         controller : function ($scope) {}
 	};
 });
